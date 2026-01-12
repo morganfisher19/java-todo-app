5b202939a3cc219a3956dffae873a3893f593ad1
morganfisher19 <morganfisher319@gmail.com>
2026-01-12 12:30:47 -0500

    creating variables for calculator

---
+++ MortgageCalculator/MortgageCalculator/src/Main.java
@@ -0,0 +1,23 @@
+import java.text.NumberFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        // User input variables
+        int inputPrincipal = 100000;
+        double inputAnnualInterestRate = .005;
+        int inputYears = 180;
+
+        // Calculate equation variables
+        int numberOfPayments = inputYears * 12;
+        double monthlyInterestRate = inputAnnualInterestRate / 12;
+
+
+        // Calculate mortgage
+        double mortgageEqtnNumerator = 
+
+        // NumberFormat currency = NumberFormat.getCurrencyInstance();
+        // String result5 = currency.format(1234567.891);
+
+        System.out.println("Mortgage: " + mortgage);
+    }
+}
